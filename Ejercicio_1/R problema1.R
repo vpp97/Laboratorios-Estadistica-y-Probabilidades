cbd3b9f47e8b3bcd459513547bb04d8c3de42765
Victor-Ponce-Pinedo <vipopi1997@gmail.com>
2018-04-10 16:29:12 -0500

    Add files via upload

---
+++ Ejercicio_1/R problema1.R
@@ -0,0 +1,11 @@
+arr=seq(5:-11,-0.3)
+arr
+arr=sort(arr)
+
+vec=seq(5,-11,-0.3)
+vec
+vec=sort(vec)
+
+vec2=c(rep(-1.3,10), rep(3,10), rep(-5,10),rep(7,10),rep(-9,10))
+vec2=rev(rep(sort(vec2),2))
+vec2
