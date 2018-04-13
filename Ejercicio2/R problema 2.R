98d009b6faff52b561d84dbe807af847060779bb
Victor-Ponce-Pinedo <vipopi1997@gmail.com>
2018-04-13 13:58:17 -0500

    Update R problema 2.R

--- Ejercicio2/R problema 2.R
+++ Ejercicio2/R problema 2.R
@@ -1,20 +1,27 @@
+
+##  ITEM a
 ar=matrix(c(4.3, 3.1, 8.2, 8.2, 3.2, 0.9, 1.6 , 6.5),nrow =4,byrow = 1) #definimos la matriz "ar"
 ar  #visualizamos la matriz
-dim(ar) #mostramos la dimensiÛn de la matriz
-#eliminaremos una fila:
+dim(ar) #mostramos la dimensi√≥n de la matriz
+#ITEM b , eliminaremos una fila:
 ar_b=ar[-1,] #primera fila eliminada
 ar_b   #mostramos la nueva matriz
-dim(ar_b)  #mostramos la dimensiÛn de la matriz
-ar[,2]=sort(ar[,2])
-ar
-ar_c=matrix(ar[-4,-1])
-ar_c
+dim(ar_b)  #mostramos la dimensi√≥n de la matriz
+#ITEM c,
+ar[,2]=sort(ar[,2]) #en la segunda columnade "ar", sobreescribimos esta misma, pero ordenada
+ar  #visualizamos
+
+#ITEM d
+ar_c=matrix(ar[-4,-1])  #eliminacion de la fila 4 y columna 1
+ar_c  #visualizamos
 
 
 
-A=matrix(c(2,0,0,0,0,3,0,0,0,0,5,0,0,0,0,-1),nrow=4,byrow=1)
-A
-B=solve(A)
-Id4=diag(1,nrow=4)
-Id4
-A %*% B - Id4
+
+#ITEM g
+A=matrix(c(2,0,0,0,0,3,0,0,0,0,5,0,0,0,0,-1),nrow=4,byrow=1)  #definimos la matriz A
+A  #visualizamos
+B=solve(A)    #en B almacenamos la inversa de A
+Id4=diag(1,nrow=4)  #definimos la identidad 4x4
+Id4  #visualizamos
+A %*% B - Id4  #comprobaci√≥n
